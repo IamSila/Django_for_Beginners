030752875345b6a452464ba23e3ba86bf96307f7
IamSila <silamulingi@gmail.com>
2024-12-20 23:26:05 +0300

    added templates for viewing my blog contents

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
+
 
 # Create your views here.
+
+#Homepage view
+class BlogListView(ListView):
+    """This will be used in our homepage"""
+    model = Post
+    template_name = 'home.html'
+    # context_object_name = 

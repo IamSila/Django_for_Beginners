7e8ca1b79f3832f0f86b53d46c225f77e0bb785a
IamSila <silamulingi@gmail.com>
2024-12-21 00:13:59 +0300

    added working links and fetched things from the db

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -12,3 +12,7 @@ class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
     # context_object_name = 
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
